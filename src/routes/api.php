d3cbd939bfcb2e5a9ff19776e020814010002766
cristian <desancheztorres@gmail.com>
2021-11-10 02:33:27 +0000

    Removed Save all doctors Controller

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,13 +1,12 @@
 <?php
 
-use App\Http\Controllers\Doctors\{CreateDoctorController, GetAllDoctorsController, SaveAllDoctorsController};
+use App\Http\Controllers\Doctors\{CreateDoctorController, GetAllDoctorsController};
 use App\Http\Controllers\DoctorSlots\{GetAllDoctorSlotsController};
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'doctors'], function () {
     Route::get('/', GetAllDoctorsController::class);
     Route::post('/', CreateDoctorController::class);
-    Route::post('/all', SaveAllDoctorsController::class);
 });
 
 Route::group(['prefix' => 'slots'], function () {
